10d1999ecd83ca26325926bebdeb603902ce6404
GongXiaoYing <956289900@qq.com>
2019-10-10 14:46:01 +0800

    新的画点

--- main_v2_create_inlier_set.py
+++ main_v2_create_inlier_set.py
@@ -9,10 +9,10 @@ from preprocessing import pre_matching
 
 import numpy as np
 # 图像路径
-img_path_r = "./img/train_set_9_20/9_r.jpg"
-img_path_s = "./img/train_set_9_20/9_s.jpg"
+img_path_r = "./img/exp_img/1_r.jpg"
+img_path_s = "./img/exp_img/1_s.jpg"
 
-constants.SIFT_THRESHOLD = 0.3
+constants.SIFT_THRESHOLD = 0.4
 pre_matches_s_strict, pre_matches_r_strict, des_s_strict, des_r_strict, img_s, img_r, resize_h, resize_w = \
     pre_matching.get_pre_matches(img_path_s, img_path_r, False)
 
@@ -22,9 +22,8 @@ pre_matches2 = np.transpose(pre_matches_s_strict)
 matching_result = np.ones(len(pre_matches1[0]))
 result = np.vstack((pre_matches2, pre_matches1, matching_result))
 
+print(len(result[0]))
+filename = img_path_s.split("/")[len(img_path_s.split("/")) - 1] + \
+           img_path_r.split("/")[len(img_path_r.split("/")) - 1] + "_"
 
-
-# filename = img_path_s.split("/")[len(img_path_s.split("/")) - 1] + \
-#            img_path_r.split("/")[len(img_path_r.split("/")) - 1] + "_"
-#
-# np.savez('./label_result/' + filename + "_0.3_i", correspondence_label=result)
+np.savez('./label_result/' + filename + "_0.3_i", correspondence_label=result)
--- main_v2_exp.py
+++ main_v2_exp.py
@@ -11,13 +11,21 @@ from label.label_show_points_pass import Label
 
 import numpy as np
 # 图像路径
-img_path_r = "./img/exp_img/1_r.jpg"
-img_path_s = "./img/exp_img/1_s.jpg"
+img_path_r = "./img/exp_img/4_r.jpg"
+img_path_s = "./img/exp_img/4_s.jpg"
 
 constants.SIFT_THRESHOLD = 1.0
 pre_matches_s_loose, pre_matches_r_loose, des_s_loose, des_r_loose, img_s, img_r, resize_h, resize_w = \
     pre_matching.get_pre_matches(img_path_s, img_path_r, False)
 
+print(len(pre_matches_s_loose))
+
+constants.SIFT_THRESHOLD = 0.4
+pre_matches_s_strict, pre_matches_r_strict, des_s_strict, des_r_strict, _, _, _, _ = \
+    pre_matching.get_pre_matches(img_path_s, img_path_r, False)
+
+_, index = utils.set_difference_2d(pre_matches_s_loose, pre_matches_s_strict)
+print(index)
 
 pre_matches1 = pre_matches_r_loose
 pre_matches2 = pre_matches_s_loose
